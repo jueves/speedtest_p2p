7cb1a94f59a291241b124153bf6c1c5a233d8fc0
Luis Cobiella Hernández <alu0100358797@ull.edu.es>
2022-09-27 15:24:48 +0100

    Uses a single speed value for plots

--- speedtest.R
+++ speedtest.R
@@ -7,35 +7,42 @@ data <- read_csv("speedtest_dummy.csv")
 # Compares uploads and downloads along time
 data %>%
   mutate(download=download/1024, upload=upload/1024) %>%
-  ggplot(aes(time)) + geom_line(aes(y=download), color="dark green") + geom_line(aes(y=upload), color="dark blue")
+  gather(key="type", value="speed", c('download', 'upload')) %>%
+  ggplot(aes(time, speed, color=type)) + geom_line()
 
 # Separate by p2p status
 data %>%
   mutate(download=download/1024, upload=upload/1024) %>%
-  ggplot(aes(time)) + geom_line(aes(y=download, color=p2p)) + geom_line(aes(y=upload, color=p2p))
+  gather(key="type", value="speed", c('download', 'upload')) %>%
+  mutate(type=str_c(type, p2p, sep="_p2p")) %>%
+  ggplot(aes(time, speed, color=type)) + geom_line()
 
 # Daily means
 data %>%
   mutate(date=as.Date(time), download=download/1024, upload=upload/1024) %>%
-  group_by(date) %>%
-  summarise(date, download=mean(download), upload=mean(upload)) %>%
+  gather(key="type", value="speed", c('download', 'upload')) %>%
+  group_by(date, type) %>%
+  summarise(date, type, speed=mean(speed)) %>%
   distinct() %>%
-  ggplot(aes(date)) + geom_line(aes(y=download), color="dark green") + geom_line(aes(y=upload), color="dark blue")
+  ggplot(aes(date, speed, color=type)) + geom_line()
 
 # Hourly means
 data %>%
   mutate(hour=hour(time), download=download/1024, upload=upload/1024) %>%
-  group_by(hour) %>%
-  summarise(hour, download=mean(download), upload=mean(upload)) %>%
+  gather(key="type", value="speed", c('download', 'upload')) %>%
+  group_by(hour, type) %>%
+  summarise(hour, speed=mean(speed)) %>%
   distinct() %>%
-  ggplot(aes(hour)) + geom_line(aes(y=download), color="dark green") + geom_line(aes(y=upload), color="dark blue")
+  ggplot(aes(hour, speed, color=type)) + geom_line()
 
 # Hourly means with p2p status
 data %>%
   mutate(hour=hour(time), download=download/1024, upload=upload/1024) %>%
-  group_by(hour, p2p) %>%
-  summarise(hour, p2p, download=mean(download), upload=mean(upload)) %>%
-  ggplot(aes(hour)) + geom_line(aes(y=download, color=p2p)) + geom_line(aes(y=upload, color=p2p))
+  gather(key="type", value="speed", c('download', 'upload')) %>%
+  mutate(type=str_c(type, p2p, sep="_p2p")) %>%
+  group_by(hour, type) %>%
+  summarise(hour, type, speed=mean(speed)) %>%
+  ggplot(aes(hour,speed, color=type)) + geom_line()
 
 # Server count
 ggplot(data, aes(`server name`, fill=`server name`)) + geom_bar() +
@@ -44,4 +51,10 @@ ggplot(data, aes(`server name`, fill=`server name`)) + geom_bar() +
 # Boxplot
 data %>%
   mutate(download=download/1024, upload=upload/1024) %>%
-  ggplot() + geom_boxplot(aes(download, color=p2p), orientation="y") + geom_boxplot(aes(upload, color=p2p), orientation="y")
+  gather(key="type", value="speed", c('download', 'upload')) %>%
+  mutate(type=str_c(type, p2p, sep="_p2p")) %>%
+  ggplot(aes(speed, color=type)) + geom_boxplot(orientation="y")
+
+
+minidata <- data[1:20, 1:ncol(data)]
+gather(minidata, key="type", value="speed", c('download', 'upload'))
